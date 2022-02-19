9cfbfe52542e5e59fa6a17a2d2284c24d87d0795
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-02-19 10:52:52 +0100

    repo: init repository
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

---
+++ packages/changelog-lib/example/changelog_lib_example.dart
@@ -0,0 +1,6 @@
+import 'package:changelog_lib/changelog_lib.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ packages/changelog-lib/lib/changelog_lib.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library changelog_lib;
+
+export 'src/changelog_lib_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.
