82a025a10a3fad1f121185a2fa7469916ccf8655
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-02-23 11:14:54 +0100

    give the possibility to apply filter to the commit header too
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- packages/changelog-lib/lib/src/filter_rule/filter_rule.dart
+++ packages/changelog-lib/lib/src/filter_rule/filter_rule.dart
@@ -18,6 +18,17 @@ class FilterRule {
   /// changelog from this filter rule
   final String nameSection;
 
+  /// if not null we make a check on the header too
+  /// by exact match we keep the commit if header and body
+  /// match are satisfied.
+  final String? headerExactMatch;
+
+  /// if not null the filter rule take into count the
+  /// regex if the exact match fails, and the commit
+  /// it keep in the section only if the header and body
+  /// will match.
+  final RegExp? headerRegex;
+
   /// The exact match need to be specified
   /// and it can contains a prefix for example
   final String exactMatch;
@@ -27,7 +38,7 @@ class FilterRule {
   /// of the match it is specified in the commit info
   final RegExp? regex;
 
-  FilterRule({required this.exactMatch, required this.nameSection, this.regex});
+  FilterRule({required this.exactMatch, required this.nameSection, this.regex, this.headerExactMatch, this.headerRegex});
 
   /// match is a function that apply the match logic to
   /// return the content to put in the changelog, and
@@ -36,9 +47,29 @@ class FilterRule {
   /// In addition the match function return the raw line
   /// and it is not able to clean the metadata attach to the commit body.
   String? match({required CommitInfo commitInfo}) {
+    var headerMatch = _headerMatch(commitInfo: commitInfo);
+    if (!headerMatch) {
+      return null;
+    }
+    return _bodyMatch(commitInfo: commitInfo);
+  }
+
+  bool _headerMatch({required CommitInfo commitInfo}) {
+    if (headerExactMatch == null && headerRegex == null) {
+      return true;
+    }
+    var header = commitInfo.content.commitHeader;
+    if (header.startsWith(headerExactMatch!)) {
+      return true;
+    }
+    var match = regex!.allMatches(header);
+    return match.isNotEmpty;
+  }
+
+  String? _bodyMatch({required CommitInfo commitInfo}) {
     var bodyLines = commitInfo.content.commitBody.split("\n");
     for (var line in bodyLines) {
-      if (line.contains(exactMatch)) {
+      if (line.startsWith(exactMatch)) {
         return line;
       }
       if (regex != null) {
