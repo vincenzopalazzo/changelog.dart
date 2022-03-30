f09ee932e559640a3cb3b501985361b810a1291c
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-03-30 11:49:50 +0200

    changelog-cmd: add another generation method
    Changelog-Added: add a metadata generator method to improve the usage of metadata inside the commit message and make the commit header more human-friendly.
    
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- packages/changelog-cmd/lib/method/generator_mediator.dart
+++ packages/changelog-cmd/lib/method/generator_mediator.dart
@@ -1,15 +1,16 @@
-import 'package:changelog_cmd/method/header_msg.dart';
-import 'package:changelog_cmd/method/method_generation.dart';
-import 'package:changelog_lib/changelog_lib.dart';
-
 /// Generator mediator contains all the generation method
 /// supported by the changelog command line
 ///
 /// author: https://github.com/vincenzopalazzo
+import 'package:changelog_cmd/method/header_msg.dart';
+import 'package:changelog_cmd/method/metadata_msg.dart';
+import 'package:changelog_cmd/method/method_generation.dart';
+import 'package:changelog_lib/changelog_lib.dart';
 
 class GeneratorMediator {
   final Map<String, MethodGenerator> _methods = {
     "header": HeaderGenerator(),
+    "metadata": MetadataGenerator(),
   };
 
   /// Check if inside the map there is a method generator with the name provided
--- packages/changelog-cmd/lib/method/method_generation.dart
+++ packages/changelog-cmd/lib/method/method_generation.dart
@@ -1,11 +1,14 @@
-import 'package:changelog_lib/changelog_lib.dart';
-
 /// Method Generator is the interface to implement different
 /// method generation for the Changelog.
 ///
 /// author: https://github.com/vincenzopalazzo
+import 'package:changelog_lib/changelog_lib.dart';
 
 abstract class MethodGenerator {
   // apply the sequence of rules to the generator
-  void apply({required ChangelogGenerator generator});
+  void apply(
+      {required ChangelogGenerator generator,
+      String? exactMatchHeader,
+      RegExp? regexHeader,
+      bool strictly = false});
 }
