8223b2e509abb602aecf0af74c38b80253614016
Vincenzo Palazzo <vincenzopalazzodev@gmail.com>
2022-07-14 00:43:26 +0100

    feat(changelog_cmd): add small entry when there are no changes
    Signed-off-by: Vincenzo Palazzo <vincenzopalazzodev@gmail.com>

--- packages/changelog-cmd/lib/printer/md/markdown_printer.dart
+++ packages/changelog-cmd/lib/printer/md/markdown_printer.dart
@@ -15,21 +15,25 @@ class MarkDownPrinter extends ChangelogPrinter {
     for (var section in changelogInfo.sections.values) {
       if (section.changes.isNotEmpty) {
         changelogContent += "\n## ${section.sectionName}\n";
-
         for (var change in section.changes) {
           changelogContent +=
               "- ${change.content} ([commit](${change.ref ?? ""})). @${change.authorInfo.gitNickname} ${DateFormat("dd-MM-yyyy").format(change.authorInfo.commitDate)}\n";
         }
       }
     }
+    if (changelogInfo.sections.values.isEmpty) {
+      changelogContent += "No change to be aware!\n";
+    }
     log(changelogContent);
     // TODO open a changelog file if exit and load the content in one tmp string
+    // TODO: add possibility to merge the beta change inside the official release, this required to parse the content of the readme in the section changelog again
+    // TODO: make a file inside the changelog that give the possibility to check the pre release pattern name
     var file = File("$fileName.md");
     if (!file.existsSync()) {
       file = await file.create(recursive: true);
     } else {
       var oldContent = await file.readAsString();
-      if (oldContent.contains(changelogInfo.versionName)) {
+      if (oldContent.contains("${changelogInfo.versionName} ")) {
         throw Exception(
             "Version ${changelogInfo.versionName} already inside the changelog");
       }
